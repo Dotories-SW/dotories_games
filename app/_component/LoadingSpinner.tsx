2aff54d0cd7857e6ca67a6a8c1b16d7f4c3a6187
KBG1 <bumdoly200@naver.com>
2025-11-18 11:10:16 +0900

    refactor : 로딩스패너에서텍스트 제거

--- app/_component/LoadingSpinner.tsx
+++ app/_component/LoadingSpinner.tsx
@@ -6,7 +6,6 @@ export default function LoadingSpinner() {
     >
       <div className="text-center">
         <div className="w-[10vw] h-[10vw] border-4 border-orange-500 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
-        <p className="text-gray-600 text-[3vw]">로딩 중...</p>
       </div>
     </div>
   );
