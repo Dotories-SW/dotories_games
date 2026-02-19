f1acce6afbb5f9bfbfc20da9840539c1bfbad01f
KBG1 <bumdoly200@naver.com>
2026-02-19 17:09:25 +0900

    design : text 줄넘어가는 현상 수정

--- app/arithmetic_game/page.tsx
+++ app/arithmetic_game/page.tsx
@@ -335,7 +335,7 @@ function ArithmeticGame() {
                   style={{
                     fontSize: `${
                       currentQuestion.text.includes("[이전 답]")
-                        ? "clamp(24px, 9vw, 36px)"
+                        ? "clamp(24px, 8vw, 36px)"
                         : "clamp(48px, 12vw, 56px)"
                     }`,
                   }}
